0c75be63d0f885a803393ff221e2760a6d41ab64
Squall Leonhart <Knight1988@gmail.com>
2015-10-19 16:26:13 +0700

    Add file class contain Unlock function.

---
+++ tests/SharpUtilityTests/IO/FileTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpUtility.Core.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace SharpUtility.Core.IO.Tests
+{
+    [TestClass()]
+    public class FileTests
+    {
+        [TestMethod()]
+        public void UnlockerLocationTest()
+        {
+            var location = File.UnlockerLocation;
+            Assert.AreEqual("C:\\Program Files\\Unlocker\\Unlocker.exe", location);
+        }
+    }
+}
