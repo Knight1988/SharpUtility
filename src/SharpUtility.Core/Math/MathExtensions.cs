2f10300f6a3289ab8f8ca3de47e3553dbf15b550
Squall Leonhart <Knight1988@gmail.com>
2015-10-30 11:11:22 +0700

    Add MathExtension, check Odd/Event methods

---
+++ src/SharpUtility.Core/Math/MathExtensions.cs
@@ -0,0 +1,127 @@
+﻿namespace SharpUtility.Core.Math
+{
+    public static class MathExtensions
+    {
+        #region IsEven
+
+        public static bool IsEven(this byte value)
+        {
+            return !value.IsOdd();
+        }
+
+
+        public static bool IsEven(this sbyte value)
+        {
+            return !value.IsOdd();
+        }
+
+        public static bool IsEven(this short value)
+        {
+            return !value.IsOdd();
+        }
+
+
+        public static bool IsEven(this ushort value)
+        {
+            return !value.IsOdd();
+        }
+
+        public static bool IsEven(this int value)
+        {
+            return !value.IsOdd();
+        }
+
+        public static bool IsEven(this uint value)
+        {
+            return !value.IsOdd();
+        }
+
+        public static bool IsEven(this long value)
+        {
+            return !value.IsOdd();
+        }
+
+        public static bool IsEven(this ulong value)
+        {
+            return !value.IsOdd();
+        }
+
+        public static bool IsEven(this float value)
+        {
+            return !value.IsOdd();
+        }
+
+        public static bool IsEven(this double value)
+        {
+            return !value.IsOdd();
+        }
+
+        public static bool IsEven(this decimal value)
+        {
+            return !value.IsOdd();
+        }
+
+        #endregion
+
+        #region IsOdd
+
+        public static bool IsOdd(this byte value)
+        {
+            return value%2 != 0;
+        }
+
+
+        public static bool IsOdd(this sbyte value)
+        {
+            return value%2 != 0;
+        }
+
+        public static bool IsOdd(this short value)
+        {
+            return value%2 != 0;
+        }
+
+
+        public static bool IsOdd(this ushort value)
+        {
+            return value%2 != 0;
+        }
+
+        public static bool IsOdd(this int value)
+        {
+            return value%2 != 0;
+        }
+
+        public static bool IsOdd(this uint value)
+        {
+            return value%2 != 0;
+        }
+
+        public static bool IsOdd(this long value)
+        {
+            return value%2 != 0;
+        }
+
+        public static bool IsOdd(this ulong value)
+        {
+            return value%2 != 0;
+        }
+
+        public static bool IsOdd(this float value)
+        {
+            return System.Math.Abs(value%2) > double.Epsilon;
+        }
+
+        public static bool IsOdd(this double value)
+        {
+            return System.Math.Abs(value%2) > double.Epsilon;
+        }
+
+        public static bool IsOdd(this decimal value)
+        {
+            return value%2 != 0;
+        }
+
+        #endregion
+    }
+}
---
+++ tests/SharpUtilityTests/Math/MathExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpUtility.Core.Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace SharpUtility.Core.Math.Tests
+{
+    [TestClass()]
+    public class MathExtensionsTests
+    {
+        [TestMethod()]
+        public void IsOddTest()
+        {
+            Assert.AreEqual(true, ((byte)1).IsOdd());
+            Assert.AreEqual(true, ((sbyte)1).IsOdd());
+            Assert.AreEqual(true, ((short)1).IsOdd());
+            Assert.AreEqual(true, ((ushort)1).IsOdd());
+            Assert.AreEqual(true, 1.IsOdd());
+            Assert.AreEqual(true, ((uint)1).IsOdd());
+            Assert.AreEqual(true, ((long)1).IsOdd());
+            Assert.AreEqual(true, ((ulong)1).IsOdd());
+            Assert.AreEqual(true, ((float)1).IsOdd());
+            Assert.AreEqual(true, ((double)1).IsOdd());
+            Assert.AreEqual(true, ((decimal)1).IsOdd());
+        }
+        
+        [TestMethod()]
+        public void IsEvenTest()
+        {
+            Assert.AreEqual(true, ((byte)2).IsEven());
+            Assert.AreEqual(true, ((sbyte)2).IsEven());
+            Assert.AreEqual(true, ((short)2).IsEven());
+            Assert.AreEqual(true, ((ushort)2).IsEven());
+            Assert.AreEqual(true, 2.IsEven());
+            Assert.AreEqual(true, ((uint)2).IsEven());
+            Assert.AreEqual(true, ((long)2).IsEven());
+            Assert.AreEqual(true, ((ulong)2).IsEven());
+            Assert.AreEqual(true, ((float)2).IsEven());
+            Assert.AreEqual(true, ((double)2).IsEven());
+            Assert.AreEqual(true, ((decimal)2).IsEven());
+        }
+    }
+}
