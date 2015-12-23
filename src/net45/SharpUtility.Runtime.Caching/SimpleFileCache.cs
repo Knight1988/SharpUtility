3b0d6a22712be85029e39ec9cdcc46027a24fd26
Squall Leonhart <Knight1988@gmail.com>
2015-12-23 15:24:30 +0700

    cleanup code

--- src/net45/SharpUtility.Runtime.Caching/SimpleFileCache.cs
+++ src/net45/SharpUtility.Runtime.Caching/SimpleFileCache.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using SharpUtility.Core.Security.Cryptography;
 
