5b88b7be1180d40dc9e0ce25f43646e2a421631c
Squall Leonhart <Knight1988@gmail.com>
2015-12-29 10:48:00 +0700

    Add GeckoWebExtension & DesignTImeHelper

---
+++ src/net45/SharpUtility.GeckoFX/GeckoWebBrowserExtentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gecko;
+
+namespace SharpUtility.GeckoFX
+{
+    public static class GeckoWebBrowserExtentions
+    {
+        public static string EvaluateScript(this GeckoWebBrowser browser, string jsString)
+        {
+            using (var autoJSContext = new AutoJSContext(browser.Window.JSContext))
+            {
+                var jsVal = autoJSContext.EvaluateScript(jsString, browser.Window.DomWindow);
+                return jsVal.ToString();
+            }
+        }
+    }
+}
---
+++ src/net45/SharpUtility.WinForm.Net45/DesignTimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace SharpUtility.WinForm
+{
+    public static class DesignTimeHelper
+    {
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                if (LicenseManager.UsageMode == LicenseUsageMode.Designtime) return true;
+
+                using (var process = Process.GetCurrentProcess())
+                {
+                    return process.ProcessName.ToLowerInvariant().Contains("devenv");
+                }
+            }
+        }
+    }
+}
