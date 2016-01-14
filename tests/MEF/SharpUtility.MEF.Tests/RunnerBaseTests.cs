08767841975fc7d30ec00f97da724391d1729198
Squall Leonhart <Knight1988@gmail.com>
2016-01-15 00:41:44 +0700

    Create test project

---
+++ tests/MEF/SharpUtility.MEF.Tests/RunnerBaseTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppDomainTestRunner;
+using NUnit.Framework;
+namespace AppDomainTestRunner.Tests
+{
+    [TestFixture()]
+    public class RunnerBaseTests
+    {
+        [Test()]
+        public void InitializeTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void InitializeTest1()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void RecomposeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
