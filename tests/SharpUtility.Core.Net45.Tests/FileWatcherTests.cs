8d0d9275510cfb4cbcaceb4bdcfbe2c8a79f698e
Squall Leonhart <Knight1988@gmail.com>
2016-01-21 14:37:13 +0700

    add COdeManager

---
+++ tests/SharpUtility.Core.Net45.Tests/FileWatcherTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using SharpUtility.Core;
+
+namespace SharpUtility.Tests
+{
+    [TestFixture]
+    public class FileWatcherTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            File.Delete(_testFile);
+        }
+
+        private readonly string _testFile = Path.Combine(Environment.CurrentDirectory, "Test.txt");
+        private readonly string _testFile2 = Path.Combine(Environment.CurrentDirectory, "Test2.txt");
+
+        [Test]
+        public void FileWatcherTest()
+        {
+            var watcher = new FileWatcher(Environment.CurrentDirectory);
+
+            // Test created
+            CodeManager.CreateCodeConfiguration()
+                .RunAfter(1000)
+                .ExecuteAsync(() => File.WriteAllText(_testFile, @"Test"));
+            watcher.WaitForChanged(WatcherChangeTypes.Created);
+
+            // Test changed
+            CodeManager.CreateCodeConfiguration()
+                .RunAfter(1000)
+                .ExecuteAsync(() => File.WriteAllText(_testFile, @"Test 2"));
+            watcher.WaitForChanged(WatcherChangeTypes.Changed);
+
+            // Test Rename
+            CodeManager.CreateCodeConfiguration()
+                .RunAfter(1000)
+                .ExecuteAsync(() => File.Move(_testFile, _testFile2));
+            watcher.WaitForChanged(WatcherChangeTypes.Renamed);
+
+            // Test Rename
+            CodeManager.CreateCodeConfiguration()
+                .RunAfter(1000)
+                .ExecuteAsync(() => File.Delete(_testFile2));
+            watcher.WaitForChanged(WatcherChangeTypes.Deleted);
+        }
+    }
+}
