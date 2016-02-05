2541a6bf5c38092ce1f86caac60a871174ae7166
Squall Leonhart <Knight1988@gmail.com>
2016-02-05 10:57:17 +0700

    Add new Replace extension

--- src/net45/SharpUtility.Core/String/StringExtensions.cs
+++ src/net45/SharpUtility.Core/String/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace SharpUtility.Core.String
@@ -51,6 +52,34 @@ namespace SharpUtility.Core.String
                              .Where(x => x % 2 == 0)
                              .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
                              .ToArray();
+        }
+
+        /// <summary>
+        /// Replace string with StringComparision
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="oldValue"></param>
+        /// <param name="newValue"></param>
+        /// <param name="comparison"></param>
+        /// <returns></returns>
+        public static string Replace(this string str, string oldValue, string newValue, StringComparison comparison)
+        {
+            var sb = new StringBuilder();
+
+            var previousIndex = 0;
+            var index = str.IndexOf(oldValue, comparison);
+            while (index != -1)
+            {
+                sb.Append(str.Substring(previousIndex, index - previousIndex));
+                sb.Append(newValue);
+                index += oldValue.Length;
+
+                previousIndex = index;
+                index = str.IndexOf(oldValue, index, comparison);
+            }
+            sb.Append(str.Substring(previousIndex));
+
+            return sb.ToString();
         }
     }
 }
