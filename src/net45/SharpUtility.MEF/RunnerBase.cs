fc6f2632e3deeae25006d942ad20293bcebdc3d2
Squall Leonhart <Knight1988@gmail.com>
2016-02-16 16:32:31 +0700

    remove auto recompose

--- src/net45/SharpUtility.MEF/RunnerBase.cs
+++ src/net45/SharpUtility.MEF/RunnerBase.cs
@@ -11,27 +11,11 @@ namespace SharpUtility.MEF
 {
     public class RunnerBase<T> : MarshalByRefObject where T : IExporterBase
     {
-        private bool _autoRecompose;
         private CompositionContainer _container;
         private DirectoryCatalog _directoryCatalog;
-        private FileSystemWatcher _watcher;
         public Dictionary<string, T> Exports { get; private set; }
 
-        // watch the plugin folder and raise event if file changed
-        public bool AutoRecompose
-        {
-            get { return _autoRecompose; }
-            set
-            {
-                _autoRecompose = value;
-                if (value) AddFileWatcher();
-                else RemoveFileWatcher();
-            }
-        }
-
         public string PluginPath { get; private set; }
-
-        private readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
 
         public void Initialize()
         {
@@ -39,15 +23,9 @@ namespace SharpUtility.MEF
             Initialize(pluginPath);
         }
 
-        public void WaitForUpdate()
-        {
-            _autoResetEvent.WaitOne();
-        }
-
         public void Initialize(string pluginPath)
         {
             PluginPath = pluginPath;
-            AutoRecompose = AutoRecompose;
 
             // Use RegistrationBuilder to set up our MEF parts.
             var regBuilder = new RegistrationBuilder();
@@ -76,100 +54,12 @@ namespace SharpUtility.MEF
             }, p => p);
         }
 
-        private void PrivateRecompose()
+        public void Recompose()
         {
             // Gimme 3 steps...
             _directoryCatalog.Refresh();
             _container.ComposeParts(_directoryCatalog.Parts);
-            var exports = GetExportedValues();
-
-            // get insert, updated
-            foreach (var pair in exports)
-            {
-                if (!Exports.ContainsKey(pair.Value.Name))
-                {
-                    // get inserted
-                    Exports.Add(pair.Key, pair.Value);
-                    OnExportUpdate(ExportUpdateEventType.Insert, pair.Value, default(T));
-                }
-            }
-
-            // get deleted
-            foreach (var pair in Exports.ToDictionary(p => p.Key, p => p.Value))
-            {
-                if (!exports.ContainsKey(pair.Value.Name))
-                {
-                    // get deleted
-                    Exports.Remove(pair.Value.Name);
-                    OnExportUpdate(ExportUpdateEventType.Delete, default(T), pair.Value);
-                }
-            }
+            Exports = GetExportedValues();
         }
-
-        public void Recompose()
-        {
-            if (AutoRecompose) throw new Exception("Cannot manual call Recompose while AutoRecompose is on.");
-
-            PrivateRecompose();
-        }
-
-        protected virtual void OnExportUpdate(ExportUpdateEventArgs<T> e)
-        {
-            _autoResetEvent.Set();
-            var handler = ExportUpdate;
-            if (handler != null) handler(this, e);
-        }
-
-        protected virtual void OnExportUpdate(ExportUpdateEventType eventType, T inserted, T deleted)
-        {
-            OnExportUpdate(new ExportUpdateEventArgs<T>(eventType, inserted, deleted));
-        }
-
-        private void AddFileWatcher()
-        {
-            if (_watcher != null || !Directory.Exists(PluginPath)) return;
-
-            _watcher = new FileSystemWatcher(PluginPath)
-            {
-                EnableRaisingEvents = true
-            };
-            _watcher.Deleted += WatcherOnChanged;
-            _watcher.Created += WatcherOnChanged;
-        }
-
-        private void RemoveFileWatcher()
-        {
-            if (_watcher == null) return;
-            _watcher.Dispose();
-            _watcher = null;
-        }
-
-        private void WatcherOnChanged(object sender, FileSystemEventArgs args)
-        {
-            PrivateRecompose();
-        }
-
-        public event EventHandler<ExportUpdateEventArgs<T>> ExportUpdate;
-    }
-
-    public class ExportUpdateEventArgs<T> : EventArgs where T : IExporterBase
-    {
-        public ExportUpdateEventType EventType { get; set; }
-        public T Inserted { get; set; }
-        public T Deleted { get; set; }
-
-        public ExportUpdateEventArgs(ExportUpdateEventType eventType, T inserted, T deleted)
-        {
-            EventType = eventType;
-            Inserted = inserted;
-            Deleted = deleted;
-        }
-    }
-
-    public enum ExportUpdateEventType
-    {
-        Insert,
-        Update,
-        Delete
     }
 }
--- tests/MEF/SharpUtility.MEF.Tests/RunnerBaseTests.cs
+++ tests/MEF/SharpUtility.MEF.Tests/RunnerBaseTests.cs
@@ -53,7 +53,7 @@ namespace AppDomainTestRunner.Tests
             File.Copy(lib1V2Path, lib1PluginPath, true);
             runner.Recompose();
             actual = runner.DoSomething();
-            expected = "Lib1-v2 Lib2";
+            expected = "Lib2 Lib1-v2";
 
             Assert.AreEqual(expected, actual);
 
@@ -68,70 +68,6 @@ namespace AppDomainTestRunner.Tests
             // Delete Lib2
             File.Delete(lib2PluginPath);
             runner.Recompose(); 
-            actual = runner.DoSomething();
-            expected = string.Empty;
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Ignore("Not complete")]
-        public void SwapDllEventTest()
-        {
-            var cachePath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "ShadowCopyCache");
-            var pluginPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "Plugins");
-            var currentDir = new DirectoryInfo(Environment.CurrentDirectory);
-            var rootPath = currentDir.Parent.Parent.Parent;
-            var lib1V1Path = Path.Combine(rootPath.FullName, @"MEFTestLib1\bin\Release\MEFTestLib1.dll");
-            var lib1V2Path = Path.Combine(rootPath.FullName, @"MEFTestLib1-v2\bin\Release\MEFTestLib1-v2.dll");
-            var lib2Path = Path.Combine(rootPath.FullName, @"MEFTestLib2\bin\Release\MEFTestLib2.dll");
-            var lib1PluginPath = Path.Combine(pluginPath, "MEFTestLib1.dll");
-            var lib2PluginPath = Path.Combine(pluginPath, "MEFTestLib2.dll");
-
-            // Clear plugin folder
-            if (Directory.Exists(pluginPath))
-            {
-                Directory.Delete(pluginPath, true);
-            }
-            Directory.CreateDirectory(pluginPath);
-            // Copy lib1
-            File.Copy(lib1V1Path, lib1PluginPath);
-
-            var runner = RunnerManager.CreateRunner<Runner, IExport>("Test", pluginPath, cachePath);
-            runner.AutoRecompose = true;
-
-            var actual = runner.DoSomething();
-            var expected = "Lib1";
-
-            Assert.AreEqual(expected, actual);
-
-            // Copy lib2
-            File.Copy(lib2Path, lib2PluginPath);
-            runner.WaitForUpdate();
-            actual = runner.DoSomething();
-            expected = "Lib1 Lib2";
-
-            Assert.AreEqual(expected, actual);
-            // Replace lib1 with v2
-            File.Delete(lib1PluginPath);
-            runner.WaitForUpdate();
-            File.Copy(lib1V2Path, lib1PluginPath, true);
-            runner.WaitForUpdate();
-            actual = runner.DoSomething();
-            expected = "Lib1-v2 Lib2";
-
-            Assert.AreEqual(expected, actual);
-
-            // Delete Lib1
-            File.Delete(lib1PluginPath);
-            runner.WaitForUpdate();
-            actual = runner.DoSomething();
-            expected = "Lib2";
-
-            Assert.AreEqual(expected, actual);
-
-            // Delete Lib2
-            File.Delete(lib2PluginPath);
-            runner.WaitForUpdate();
             actual = runner.DoSomething();
             expected = string.Empty;
 
