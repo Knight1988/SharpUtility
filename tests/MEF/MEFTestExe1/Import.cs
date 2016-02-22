1962cef8a7b1784d8092368b70055f3c5ba2aa7f
Squall Leonhart <Knight1988@gmail.com>
2016-02-22 11:11:40 +0700

    add exeProject

---
+++ tests/MEF/MEFTestExe1/Import.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Remoting.Lifetime;
+using MEFInterface;
+using SharpUtility.Runtime.Remoting;
+
+namespace MEFTestLib1
+{
+    public class Import : Sponsor, IExport
+    {
+        public string InHere()
+        {
+            return "Exe1";
+        }
+
+        public string Name { get; set; }
+    }
+}
