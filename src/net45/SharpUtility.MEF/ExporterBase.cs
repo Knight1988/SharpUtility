eec5fff76d0788a3f0220b8fd982bd662175ad82
Squall Leonhart <Knight1988@gmail.com>
2016-02-22 09:24:21 +0700

    Add ExporteBase

---
+++ src/net45/SharpUtility.MEF/ExporterBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpUtility.Runtime.Remoting;
+
+namespace SharpUtility.MEF
+{
+    public abstract class ExporterBase : Sponsor, IExporterBase
+    {
+        public string Name { get; set; }
+    }
+    public abstract class ExporterBase<TEventHandler> : Sponsor, IExporterBase<TEventHandler> where TEventHandler : MarshalByRefObject
+    {
+        public string Name { get; set; }
+        public abstract TEventHandler EventHandler { get; set; }
+    }
+}
