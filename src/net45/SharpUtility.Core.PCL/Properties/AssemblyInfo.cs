97ad3c9887a41a002c4d0f2c33ed57c6ae5ab24d
Squall Leonhart <Knight1988@gmail.com>
2016-02-25 10:29:13 +0700

    change AssemblyInfo.Shared location

--- src/net45/SharpUtility.Core.PCL/Properties/AssemblyInfo.cs
+++ src/net45/SharpUtility.Core.PCL/Properties/AssemblyInfo.cs
@@ -8,23 +8,4 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("SharpUtility.Core.PCL")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpUtility.Core.PCL")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
