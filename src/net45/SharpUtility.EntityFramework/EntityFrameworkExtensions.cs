a1d12428c67143e838a73d56067fdf9885c6725e
Squall Leonhart <Knight1988@gmail.com>
2016-02-26 11:19:36 +0700

    project EntityFramework init

---
+++ src/net45/SharpUtility.EntityFramework/EntityFrameworkExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity.Core.EntityClient;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace SharpUtility.EntityFramework
+{
+    public static class EntityFrameworkExtensions
+    {
+        public static EntityConnection ToEntityConnection(this SqlConnection sqlConnection)
+        {
+            var workspace = new MetadataWorkspace(new[] {"res://*/"}, new[] {Assembly.GetExecutingAssembly()});
+
+            return new EntityConnection(workspace, sqlConnection);
+        }
+    }
+}
