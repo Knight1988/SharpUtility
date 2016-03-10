f36e03066ad36e84b8bc3322bbb66aa00a4be2a3
Squall Leonhart <Knight1988@gmail.com>
2016-03-10 14:11:11 +0700

    add mor method to ConsoleExtended

--- src/net45/SharpUtility.Core/ConsoleExtended.cs
+++ src/net45/SharpUtility.Core/ConsoleExtended.cs
@@ -4,30 +4,69 @@ namespace SharpUtility
 {
     public static class ConsoleExtended
     {
-        private static readonly object _lock = new object();
+        private static readonly object Lock = new object();
+
+        /// <summary>
+        /// Write percent number
+        /// </summary>
+        /// <param name="value">current value</param>
+        /// <param name="total">total</param>
         public static void WritePercent(double value, double total)
         {
             WritePercent(value/total);
         }
 
+        /// <summary>
+        /// Write percent number
+        /// </summary>
+        /// <param name="value"></param>
         public static void WritePercent(double value)
         {
-            lock (_lock)
+            lock (Lock)
             {
-                var output = $"{value:P}    ";
+                var output = $"{value:P}\t";
                 Console.Write(output);
                 Console.SetCursorPosition(Console.CursorLeft - output.Length, Console.CursorTop);
             }
         }
 
+        /// <summary>
+        /// Write percent number line
+        /// </summary>
+        /// <param name="value"></param>
         public static void WritePercentLine(double value)
         {
             Console.WriteLine("{0:P}", value);
         }
 
+        /// <summary>
+        /// Write percent number line
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="total"></param>
         public static void WritePercentLine(double value, double total)
         {
             WritePercentLine(value/total);
+        }
+
+        /// <summary>
+        /// Write a text with x position and max length
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="left"></param>
+        /// <param name="maxLength"></param>
+        /// <param name="arg"></param>
+        public static void Write(string format, int left, int maxLength, params object[] arg)
+        {
+            var top = Console.CursorTop;
+            Console.SetCursorPosition(left, top);
+
+            if (format.Length > maxLength)
+            {
+                format = format.Substring(maxLength - format.Length);
+            }
+
+            Console.Write(format, arg);
         }
     }
 }
