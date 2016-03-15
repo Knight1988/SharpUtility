cf5054084b2ef9b95bddf0ddd5833ba6776c4450
Squall Leonhart <Knight1988@gmail.com>
2016-03-15 16:26:41 +0700

    move to Enum namespace

---
+++ src/net45/SharpUtility.Core/Enum/StringValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SharpUtility.Enum
+{
+    public class StringValue : Attribute
+    {
+        public string Value { get; }
+
+        public StringValue(string value)
+        {
+            Value = value;
+        }
+    }
+}
