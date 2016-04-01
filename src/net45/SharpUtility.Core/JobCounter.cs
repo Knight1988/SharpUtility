1630e94150e137ae6202929ca866c7bf0d1c60d5
Squall Leonhart <Knight1988@gmail.com>
2016-04-01 17:28:28 +0700

    add JobCounter

---
+++ src/net45/SharpUtility.Core/JobCounter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SharpUtility
+{
+    public class JobCounter
+    {
+        private string _lastOutput;
+
+        public long MaxValue { get; set; }
+
+        public long Value { get; set; }
+
+        public JobCounter(long value, long maxValue)
+        {
+            Value = value;
+            MaxValue = maxValue;
+        }
+
+        public JobCounter(long maxValue) : this(0, maxValue)
+        {
+        }
+
+        public JobCounter DisplayToConsole()
+        {
+            var value = Value/MaxValue;
+            var output = $"{value:P}";
+            Console.SetCursorPosition(Console.CursorLeft - _lastOutput.Length, Console.CursorTop);
+            Console.Write(output);
+            _lastOutput = output;
+            return this;
+        }
+
+        public JobCounter IncreaseValue()
+        {
+            Value++;
+            return this;
+        }
+    }
+}
