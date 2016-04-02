540383700e7037194ea5c6a227805abd29c0fdf3
Squall Leonhart <Knight1988@gmail.com>
2016-04-02 07:09:12 +0700

    add Tests

---
+++ tests/SharpUtility.Core.Net45.Tests/InputSimulate/MouseSimulatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+using SharpUtility.InputSimulate;
+
+namespace SharpUtility.Core.Tests.InputSimulate
+{
+    [TestFixture]
+    class MouseSimulatorTests
+    {
+        [Test]
+        public Task MouseClickTests()
+        {
+            /* Atc */
+            await MouseSimulator.ClickAsync()
+        }
+    }
+}
