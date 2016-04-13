4d8f03627f772b7e5a9dd4dc209d7acce7973ace
Squall Leonhart <Knight1988@gmail.com>
2016-04-13 09:14:56 +0700

    update database backup file

--- tests/SharpUtility.EntityFramework.Tests/EntityFrameworkExtensionsTests.cs
+++ tests/SharpUtility.EntityFramework.Tests/EntityFrameworkExtensionsTests.cs
@@ -3,7 +3,10 @@ using NUnit.Framework;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net;
+using Ionic.Zip;
 
 namespace SharpUtility.EntityFramework.Tests
 {
@@ -16,10 +19,22 @@ namespace SharpUtility.EntityFramework.Tests
         public void Setup()
         {
             var dataFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+            var zipPath = Path.Combine(dataFolder, "AdventureWorks2008R2-Full Database Backup.zip");
             var backupFile = Path.Combine(dataFolder, "AdventureWorks2008R2-Full Database Backup.bak");
             var dataFile = Path.Combine(dataFolder, "AdventureWorks2008R2.mdf");
             var logFile = Path.Combine(dataFolder, "AdventureWorks2008R2.ldf");
             var sql = $@"RESTORE DATABASE [AdventureWorks2008R2] FROM  DISK = N'{backupFile}' WITH  FILE = 1,  MOVE N'AdventureWorks2008R2_Data' TO N'{dataFile}',  MOVE N'AdventureWorks2008R2_Log' TO N'{logFile}',  NOUNLOAD,  REPLACE,  STATS = 10";
+
+            // Extract backup file
+            using (ZipFile zip = ZipFile.Read(zipPath))
+            {
+                // here, we extract every entry, but we could extract conditionally
+                // based on entry name, size, date, checkbox status, etc.  
+                foreach (ZipEntry e in zip)
+                {
+                    e.Extract(dataFolder, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
 
             using (var cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["AdventureWorkConnection"].ConnectionString))
             {
