19b397ba1affa9e29326bb2f01d4c35c3fbc4c4d
Squall Leonhart <Knight1988@gmail.com>
2016-05-10 17:55:58 +0700

    add CodeConfigExtensions

---
+++ src/net45/SharpUtility.Core.PCL/Common/CodeConfiguration/CodeConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpUtility.Common
+{
+    public static class CodeConfigurationExtensions
+    {
+        public static CodeConfigurationAction Try(this Action action, int maxRetries)
+        {
+            return CodeConfiguration.Try(action, maxRetries);
+        }
+
+        public static CodeConfigurationActionAsync Try(this Func<Task> action, int maxRetries, int delay)
+        {
+            return CodeConfiguration.Try(action, maxRetries, delay);
+        }
+
+        public static CodeConfigurationFunction<TResult> Try<TResult>(this Func<TResult> action, int maxRetries)
+        {
+            return CodeConfiguration.Try(action, maxRetries);
+        }
+
+        public static CodeConfigurationFunctionAsync<TResult> Try<TResult>(this Func<Task<TResult>> action, int maxRetries, int delay)
+        {
+            return CodeConfiguration.Try(action, maxRetries, delay);
+        }
+    }
+}
