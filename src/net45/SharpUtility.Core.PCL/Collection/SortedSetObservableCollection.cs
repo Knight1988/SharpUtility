31336394cbf79d5e111c1e8cdf08b63704439a72
Squall Leonhart <Knight1988@gmail.com>
2016-05-24 17:07:47 +0700

    add SortedSetObservableCollection

---
+++ src/net45/SharpUtility.Core.PCL/Collection/SortedSetObservableCollection.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using SharpUtility.Annotations;
+
+namespace SharpUtility.Collection
+{
+    public class SortedSetObservableCollection<T> : ICollection<T>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        private readonly SortedSet<T> _sortedSet;
+
+        public SortedSetObservableCollection()
+        {
+            _sortedSet = new SortedSet<T>();
+        }
+
+        public SortedSetObservableCollection(IComparer<T> comparer)
+        {
+            _sortedSet = new SortedSet<T>(comparer);
+        }
+
+        public SortedSetObservableCollection(IEnumerable<T> collection)
+        {
+            _sortedSet = new SortedSet<T>(collection);
+        }
+
+        public SortedSetObservableCollection(IEnumerable<T> collection, IComparer<T> comparer)
+        {
+            _sortedSet = new SortedSet<T>(collection, comparer);
+        }
+
+        public bool IsSynchronized { get; } = false;
+        public object SyncRoot { get; } = null;
+
+        public void RemoveWhere(Predicate<T> match)
+        {
+            var removed = _sortedSet.Where(p => match(p)).ToList();
+            _sortedSet.RemoveWhere(match);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removed));
+        }
+
+        public bool Remove(T item)
+        {
+            var result = _sortedSet.Remove(item);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item));
+            return result;
+        }
+
+        public int Count => _sortedSet.Count;
+        public bool IsReadOnly { get; } = false;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _sortedSet.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            var matchItem = _sortedSet.SingleOrDefault(p => _sortedSet.Comparer.Compare(p, item) == 0);
+            bool success;
+            if (matchItem == null)
+            {
+                success = _sortedSet.Add(item);
+                if (success) OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item));
+                return;
+            }
+
+            _sortedSet.Remove(matchItem);
+            success = _sortedSet.Add(item);
+            if (success) OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, item, matchItem));
+        }
+
+        public void Clear()
+        {
+            _sortedSet.Clear();
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public bool Contains(T item)
+        {
+            return _sortedSet.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _sortedSet.CopyTo(array, arrayIndex);
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        public void CopyTo(Array array, int index)
+        {
+            _sortedSet.CopyTo((T[])array, index);
+        }
+
+        protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            CollectionChanged?.Invoke(this, e);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- src/net45/SharpUtility.Core.PCL/Threading/BackgroundWorker/BackgroundWorker.cs
+++ src/net45/SharpUtility.Core.PCL/Threading/BackgroundWorker/BackgroundWorker.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using Nito.AsyncEx;
+using Nito.AsyncEx.Synchronous;
 
 namespace SharpUtility.Threading
 {
@@ -13,17 +16,34 @@ namespace SharpUtility.Threading
         /// </summary>
         /// <param name="action"></param>
         /// <param name="onRetry"></param>
-        public async void ExecuteAsync(Func<Task> action, Func<Task> onRetry)
+        public async Task<T> ExecuteAsync<T>(Func<Task<T>> action, Func<Exception, Task> onRetry)
         {
             Re: try
             {
-                await action();
+                return await action();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                await onRetry();
+                await onRetry(ex);
                 await _autoResetEvent.WaitAsync();
                 goto Re;
+            }
+        }
+
+        /// <summary>
+        /// Try execute action
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="onException"></param>
+        public async Task<T> ExecuteAsync<T>(Func<Task<T>> action, Func<Exception, Task<T>> onException)
+        {
+            try
+            {
+                return await action();
+            }
+            catch (Exception ex)
+            {
+                return await onException(ex);
             }
         }
 
@@ -119,6 +139,164 @@ namespace SharpUtility.Threading
         {
             return ProgressPercentage.ToString("P");
         }
+    }
+
+    public sealed class AsyncAutoResetEvent
+    {
+        /// <summary>
+        /// The queue of TCSs that other tasks are awaiting.
+        /// </summary>
+        private readonly IAsyncWaitQueue<object> _queue;
+
+        /// <summary>
+        /// The current state of the event.
+        /// </summary>
+        private bool _set;
+
+        /// <summary>
+        /// The semi-unique identifier for this instance. This is 0 if the id has not yet been created.
+        /// </summary>
+        private int _id;
+
+        /// <summary>
+        /// The object used for mutual exclusion.
+        /// </summary>
+        private readonly object _mutex;
+
+        /// <summary>
+        /// Creates an async-compatible auto-reset event.
+        /// </summary>
+        /// <param name="set">Whether the auto-reset event is initially set or unset.</param>
+        /// <param name="queue">The wait queue used to manage waiters.</param>
+        public AsyncAutoResetEvent(bool set, IAsyncWaitQueue<object> queue)
+        {
+            _queue = queue;
+            _set = set;
+            _mutex = new object();
+            //if (set)
+            //    Enlightenment.Trace.AsyncAutoResetEvent_Set(this);
+        }
+
+        /// <summary>
+        /// Creates an async-compatible auto-reset event.
+        /// </summary>
+        /// <param name="set">Whether the auto-reset event is initially set or unset.</param>
+        public AsyncAutoResetEvent(bool set)
+            : this(set, new DefaultAsyncWaitQueue<object>())
+        {
+        }
+
+        /// <summary>
+        /// Creates an async-compatible auto-reset event that is initially unset.
+        /// </summary>
+        public AsyncAutoResetEvent()
+          : this(false, new DefaultAsyncWaitQueue<object>())
+        {
+        }
+
+        /// <summary>
+        /// Whether this event is currently set. This member is seldom used; code using this member has a high possibility of race conditions.
+        /// </summary>
+        public bool IsSet
+        {
+            get { lock (_mutex) return _set; }
+        }
+
+        /// <summary>
+        /// Asynchronously waits for this event to be set. If the event is set, this method will auto-reset it and return immediately, even if the cancellation token is already signalled. If the wait is canceled, then it will not auto-reset this event.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token used to cancel this wait.</param>
+        public Task WaitAsync(CancellationToken cancellationToken)
+        {
+            Task ret;
+            lock (_mutex)
+            {
+                if (_set)
+                {
+                    _set = false;
+                    ret = TaskConstants.Completed;
+                }
+                else
+                {
+                    ret = _queue.Enqueue(_mutex, cancellationToken);
+                }
+                //Enlightenment.Trace.AsyncAutoResetEvent_TrackWait(this, ret);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Synchronously waits for this event to be set. If the event is set, this method will auto-reset it and return immediately, even if the cancellation token is already signalled. If the wait is canceled, then it will not auto-reset this event. This method may block the calling thread.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token used to cancel this wait.</param>
+        public void Wait(CancellationToken cancellationToken)
+        {
+            Task ret;
+            lock (_mutex)
+            {
+                if (_set)
+                {
+                    _set = false;
+                    return;
+                }
+
+                ret = _queue.Enqueue(_mutex, cancellationToken);
+            }
+
+            ret.WaitAndUnwrapException();
+        }
+
+        /// <summary>
+        /// Asynchronously waits for this event to be set. If the event is set, this method will auto-reset it and return immediately.
+        /// </summary>
+        public Task WaitAsync()
+        {
+            return WaitAsync(CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Synchronously waits for this event to be set. If the event is set, this method will auto-reset it and return immediately. This method may block the calling thread.
+        /// </summary>
+        public void Wait()
+        {
+            Wait(CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Sets the event, atomically completing a task returned by <see cref="o:WaitAsync"/>. If the event is already set, this method does nothing.
+        /// </summary>
+        public void Set()
+        {
+            IDisposable finish = null;
+            lock (_mutex)
+            {
+                //Enlightenment.Trace.AsyncAutoResetEvent_Set(this);
+                if (_queue.IsEmpty)
+                    _set = true;
+                else
+                    finish = _queue.Dequeue();
+            }
+            if (finish != null)
+                finish.Dispose();
+        }
+
+        // ReSharper disable UnusedMember.Local
+        [DebuggerNonUserCode]
+        private sealed class DebugView
+        {
+            private readonly AsyncAutoResetEvent _are;
+
+            public DebugView(AsyncAutoResetEvent are)
+            {
+                _are = are;
+            }
+
+            public bool IsSet { get { return _are._set; } }
+
+            public IAsyncWaitQueue<object> WaitQueue { get { return _are._queue; } }
+        }
+        // ReSharper restore UnusedMember.Local
     }
 
     public class WorkerPolicy
