d57187e50fa1cc3299eb61300fbd92109f44cf36
Squall Leonhart <Knight1988@gmail.com>
2016-10-05 14:26:35 +0700

    add WindowServiceManager

---
+++ src/net45/SharpUtility.WindowService/ServiceSettings.cs
@@ -0,0 +1,44 @@
+﻿using System.ServiceProcess;
+
+namespace SharpUtility.WindowService
+{
+    public class ServiceSettings
+    {
+        /// <summary>
+        /// Window username
+        /// </summary>
+        public string Username { get; set; }
+        /// <summary>
+        /// Window password
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Full path to file you want to install as service
+        /// </summary>
+        public string FileName { get; set; }
+        /// <summary>
+        /// Service's display name
+        /// </summary>
+        public string DisplayName { get; set; }
+        /// <summary>
+        /// Service's description
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Service's name
+        /// </summary>
+        public string ServiceName { get; set; }
+        /// <summary>
+        /// Create a log file after installing service
+        /// </summary>
+        public string LogPath { get; set; }
+        /// <summary>
+        /// Set the service password
+        /// </summary>
+        public ServiceStartMode StartMode { get; set; } = ServiceStartMode.Automatic;
+        /// <summary>
+        /// Service account which the service will run
+        /// </summary>
+        public ServiceAccount Account { get; set; } = ServiceAccount.LocalSystem;
+    }
+}
---
+++ src/net45/SharpUtility.WindowService/WindowServiceManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Specialized;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace SharpUtility.WindowService
+{
+    public static class WindowServiceManager
+    {
+        /// <summary>
+        /// Uninstall a service
+        /// </summary>
+        /// <param name="serviceName">Service name</param>
+        /// <param name="logPath">(optional) Log path</param>
+        public static void Uninstall(string serviceName, string logPath = null)
+        {
+            var serviceInstallerObj = new ServiceInstaller();
+            var context = new InstallContext(logPath, null);
+            serviceInstallerObj.Context = context;
+            serviceInstallerObj.ServiceName = serviceName;
+            serviceInstallerObj.Uninstall(null);
+        }
+
+        /// <summary>
+        /// Install a service
+        /// </summary>
+        /// <param name="serviceName">Service name</param>
+        /// <param name="displayName">Display name</param>
+        /// <param name="description">Description</param>
+        /// <param name="fileName">file name</param>
+        /// <param name="settings">(optional) more settings</param>
+        public static void Install(string serviceName, string displayName, string description, string fileName, ServiceSettings settings)
+        {
+            if (settings == null)
+            {
+                settings = new ServiceSettings
+                {
+                    ServiceName = serviceName,
+                    DisplayName = displayName,
+                    Description = description,
+                    FileName = fileName
+                };
+            }
+            Install(settings);
+        }
+
+        /// <summary>
+        /// Install a service
+        /// </summary>
+        /// <param name="settings">more settings</param>
+        public static void Install(ServiceSettings settings)
+        {
+            var procesServiceInstaller = new ServiceProcessInstaller
+            {
+                Account = settings.Account,
+                Username = settings.Username,
+                Password = settings.Password
+            };
+
+            var serviceInstallerObj = new ServiceInstaller();
+            var path = $"/assemblypath={settings.FileName}";
+            string[] cmdline = {path};
+
+            serviceInstallerObj.Context = new InstallContext(null, cmdline);
+            serviceInstallerObj.DisplayName = settings.DisplayName;
+            serviceInstallerObj.Description = settings.Description;
+            serviceInstallerObj.ServiceName = settings.ServiceName;
+            serviceInstallerObj.StartType = settings.StartMode;
+            serviceInstallerObj.Parent = procesServiceInstaller;
+
+            var state = new ListDictionary();
+            serviceInstallerObj.Install(state);
+        }
+    }
+}
