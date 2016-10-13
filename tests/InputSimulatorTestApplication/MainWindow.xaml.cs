1a44655eba2a5a9c84bf850cbfe278b1c40088b9
Squall Leonhart <Knight1988@gmail.com>
2016-10-13 16:15:24 +0700

    add InputSimulatorTestApplication

---
+++ tests/InputSimulatorTestApplication/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace InputSimulatorTestApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CenterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            CenterButton.Visibility = Visibility.Hidden;
+        }
+
+        private void MainWindow_OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            lblPosition.Content = p.ToString();
+        }
+    }
+}
