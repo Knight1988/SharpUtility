8799746225b0475af444cc0dddf3943dec0f4f13
Squall Leonhart <Knight1988@gmail.com>
2016-10-17 15:32:48 +0700

    Ignore in AppVeyor

--- tests/SharpUtility.Core.Net45.Tests/WindowService/WindowServiceManagerTests.cs
+++ tests/SharpUtility.Core.Net45.Tests/WindowService/WindowServiceManagerTests.cs
@@ -3,10 +3,10 @@ using SharpUtility.WindowService;
 
 namespace SharpUtility.Core.Tests.WindowService
 {
-    [TestFixture, Category("CloudIgnore")]
+    [TestFixture]
     public class WindowServiceManagerTests
     {
-        [Test]
+        [Test, Category("CloudIgnore")]
         public void InstallTest()
         {
             WindowServiceManager.Install("TestService", "TestService", "TestService");
@@ -16,7 +16,7 @@ namespace SharpUtility.Core.Tests.WindowService
             Assert.True(isInstalled);
         }
 
-        [Test]
+        [Test, Category("CloudIgnore")]
         public void UninstallTest()
         {
             WindowServiceManager.Uninstall("TestService");
