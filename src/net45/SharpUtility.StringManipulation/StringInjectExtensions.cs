fe769d2fe7a9d7840e9e9ab217e81d60ad559423
Squall Leonhart <Knight1988@gmail.com>
2016-10-17 17:32:54 +0700

    move to StringManipulation

---
+++ src/net45/SharpUtility.StringManipulation/StringInjectExtensions.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace SharpUtility.StringManipulation
+{
+    public static class StringInjectExtension
+    {
+        private static IDictionary GetPropertyHash(object properties)
+        {
+            return properties?.GetType()
+                .GetRuntimeProperties()
+                .ToDictionary(property => property.Name, property => property.GetValue(properties));
+        }
+
+        /// <summary>
+        ///     Replaces the format item in a specified string with the string representation
+        ///     of a corresponding properties in a specified object.
+        /// </summary>
+        /// <param name="formatString">A composite format string.</param>
+        /// <param name="injectionObjects">An array object that contain property to format</param>
+        /// <returns></returns>
+        public static string Inject(this string formatString, params object[] injectionObjects)
+        {
+            return injectionObjects.Aggregate(formatString, Inject);
+        }
+
+        /// <summary>
+        ///     Replaces the format item in a specified string with the string representation
+        ///     of a corresponding properties in a specified object.
+        /// </summary>
+        /// <param name="formatString">A composite format string.</param>
+        /// <param name="injectionObject">A object that contain property to format</param>
+        /// <returns></returns>
+        public static string Inject(this string formatString, object injectionObject)
+        {
+            return formatString.Inject(GetPropertyHash(injectionObject));
+        }
+
+        /// <summary>
+        ///     Replaces the format item in a specified string with the string representation
+        ///     of a corresponding value in a specified key.
+        /// </summary>
+        /// <param name="formatString">A composite format string.</param>
+        /// <param name="attributes">A dictionary that contain key and value to format</param>
+        /// <returns></returns>
+        public static string Inject(this string formatString, IDictionary attributes)
+        {
+            return attributes.Keys.Cast<string>().Aggregate(formatString, (current, key) => Inject(current, key, attributes[key]));
+        }
+
+        /// <summary>
+        ///     Replaces the format item in a specified string with the string representation
+        ///     of a corresponding value in a specified key.
+        /// </summary>
+        /// <param name="formatString">A composite format string.</param>
+        /// <param name="key">key to replace</param>
+        /// <param name="replacementValue">value to replace</param>
+        /// <returns></returns>
+        public static string Inject(this string formatString, string key, object replacementValue)
+        {
+            var str = formatString;
+            var regex = new Regex(string.Concat("{(", key, ")(?:}|(?::(.[^}]*)}))"));
+            foreach (Match match in regex.Matches(formatString))
+            {
+                string str1;
+                if (match.Groups[2].Length <= 0)
+                {
+                    var empty = replacementValue ?? string.Empty;
+                    str1 = empty.ToString();
+                }
+                else
+                {
+                    var invariantCulture = CultureInfo.InvariantCulture;
+                    object[] item = {match.Groups[2]};
+                    var str2 = string.Format(invariantCulture, "{{0:{0}}}", item);
+                    var currentCulture = CultureInfo.CurrentCulture;
+                    item = new[] {replacementValue};
+                    str1 = string.Format(currentCulture, str2, item);
+                }
+                str = str.Replace(match.ToString(), str1);
+            }
+            return str;
+        }
+    }
+}
---
+++ tests/SharpUtility.Core.Net45.Tests/StringManipulation/StringInjectExtensionTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using SharpUtility.StringManipulation;
+
+namespace SharpUtility.Core.Tests.StringManipulation
+{
+    [TestFixture]
+    public class StringInjectExtensionTests
+    {
+        [Test]
+        public void InjectDictionaryTest()
+        {
+            /* Arrange */
+            const string template = "{Prefix}{FirstName} {LastName}";
+            var dic = new Dictionary<string, string>
+            {
+                {"Prefix", "Mr."},
+                {"FirstName", "Squall"},
+                {"LastName", "Leonhart"}
+            };
+
+            /* Act */
+            var actual = template.Inject(dic);
+
+            /* Assert */
+            actual.Should().Be("Mr.Squall Leonhart");
+        }
+
+        [Test]
+        public void InjectManyObjectTest()
+        {
+            /* Arrange */
+            const string template = "{Prefix}{FirstName} {LastName}";
+            var obj1 = new {FirstName = "Squall"};
+            var obj2 = new {Prefix = "Mr.", LastName = "Leonhart"};
+
+            /* Act */
+            var actual = template.Inject(obj1, obj2);
+
+            /* Assert */
+            actual.Should().Be("Mr.Squall Leonhart");
+        }
+
+        [Test]
+        public void InjectObjectTest()
+        {
+            /* Arrange */
+            const string template = "{Prefix}{FirstName} {LastName}";
+            var obj = new
+            {
+                Prefix = "Mr.",
+                FirstName = "Squall",
+                LastName = "Leonhart"
+            };
+
+            /* Act */
+            var actual = template.Inject(obj);
+
+            /* Assert */
+            actual.Should().Be("Mr.Squall Leonhart");
+        }
+
+        [Test]
+        public void InjectSingleValueTest()
+        {
+            /* Arrange */
+            const string template = "{Prefix}{FirstName} {LastName}";
+
+            /* Act */
+            var actual = template.Inject("Prefix", "Mr.").Inject("FirstName", "Squall").Inject("LastName", "Leonhart");
+
+            /* Assert */
+            actual.Should().Be("Mr.Squall Leonhart");
+        }
+    }
+}
