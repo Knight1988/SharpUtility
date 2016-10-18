c8a5cd459ba67bbdca5be1358d634e020a5a72b2
Squall Leonhart <Knight1988@gmail.com>
2016-10-18 14:48:36 +0700

    Remove InternetRequired category

--- tests/SharpUtility.Core.Net45.Tests/Mail/MailPingerTests.cs
+++ tests/SharpUtility.Core.Net45.Tests/Mail/MailPingerTests.cs
@@ -1,29 +1,36 @@
 ﻿using System.Threading.Tasks;
+using FluentAssertions;
 using NUnit.Framework;
 using SharpUtility.Mail;
 
 namespace SharpUtility.Core.Tests.Mail
 {
-    [TestFixture(Category = "InternetRequired")]
+    [TestFixture]
     public class MailPingerTests
     {
-        [Test]
         [TestCase("wellchoice.an@gmail.com")]
         [TestCase("kemvinh@gmail.com")]
         public async Task PingTestValid(string email)
         {
+            /* Arrange */
+
+            /* Act */
             var actual = await MailPinger.PingAsync(email, "gmail-smtp-in.l.google.com");
 
-            Assert.AreEqual(PingStatus.Valid, actual);
+            /* Assert */
+            actual.Should().Be(PingStatus.Valid);
         }
-
-        [Test]
+        
         [TestCase("not.a.valid.emailaddress.1234qa@gmail.com")]
         public async Task PingTestInvalid(string email)
         {
+            /* Arrange */
+
+            /* Act */
             var actual = await MailPinger.PingAsync(email, "gmail-smtp-in.l.google.com");
 
-            Assert.AreEqual(PingStatus.Invalid, actual);
+            /* Assert */
+            actual.Should().Be(PingStatus.Invalid);
         }
     }
 }
