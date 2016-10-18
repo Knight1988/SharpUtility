cb4e15f2a2afb222a1b0d15d4fb38dd00bcfe9c8
Squall Leonhart <Knight1988@gmail.com>
2016-10-18 14:46:30 +0700

    move Geckofx lib

--- tests/SharpUtility.Core.Net45.Tests/WindowService/WindowServiceManagerTests.cs
+++ tests/SharpUtility.Core.Net45.Tests/WindowService/WindowServiceManagerTests.cs
@@ -3,10 +3,10 @@ using SharpUtility.WindowService;
 
 namespace SharpUtility.Core.Tests.WindowService
 {
-    [TestFixture]
+    [TestFixture(Category = "CloudIgnore")]
     public class WindowServiceManagerTests
     {
-        [Test, Category("CloudIgnore")]
+        [Test]
         public void InstallTest()
         {
             WindowServiceManager.Install("TestService", "TestService", "TestService");
@@ -16,7 +16,7 @@ namespace SharpUtility.Core.Tests.WindowService
             Assert.True(isInstalled);
         }
 
-        [Test, Category("CloudIgnore")]
+        [Test]
         public void UninstallTest()
         {
             WindowServiceManager.Uninstall("TestService");
