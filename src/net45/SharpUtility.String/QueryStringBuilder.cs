4486346c011648e78ec21a6e8c535218de10d4d7
Squall Leonhart <Knight1988@gmail.com>
2016-10-18 10:34:46 +0700

    fix QueryStringBuilder

--- src/net45/SharpUtility.String/QueryStringBuilder.cs
+++ src/net45/SharpUtility.String/QueryStringBuilder.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 
 namespace SharpUtility.String
 {
     /// <summary>
-    /// Build a query string
-    /// Add key & value then call ToString to make a querystring
+    ///     Build a query string
+    ///     Add key & value then call ToString to make a querystring
     /// </summary>
     public class QueryStringBuilder : Dictionary<string, object>
     {
         public override string ToString()
         {
             var array = from p in this
-                select $"{Uri.EscapeDataString(p.Key)}={Uri.EscapeDataString(p.Value.ToString())}";
+                select
+                $"{Uri.EscapeDataString(p.Key).Replace("%20", "+")}={Uri.EscapeDataString(p.Value.ToString()).Replace("%20", "+")}";
             return "?" + string.Join("&", array);
         }
     }
